86020fbe4a97ffbaae7bff8b663a0792d637811e
Jonathan Guyer <guyer@nist.gov>
2021-01-28 22:24:37 -0500

    Implement robust import of all classes into package
    Linters complained about all the `from .module import *`.
    This is arguably no safer, and definitely more opaque, but
    it's robust and lintalicious.

--- steppyngstounes/__init__.py
+++ steppyngstounes/__init__.py
@@ -1,20 +1,30 @@
-from . import checkpointStepper
-from .checkpointStepper import *
-from . import fixedStepper
-from .fixedStepper import *
-from . import pidStepper
-from .pidStepper import *
-from . import pseudoRKQSStepper
-from .pseudoRKQSStepper import *
-from . import scaledStepper
-from .scaledStepper import *
-from . import sequenceStepper
-from .sequenceStepper import *
+from inspect import isclass
+from pkgutil import iter_modules
+from os.path import (abspath, dirname)
+from importlib import import_module
 
-__all__ = []
-__all__.extend(checkpointStepper.__all__)
-__all__.extend(fixedStepper.__all__)
-__all__.extend(pidStepper.__all__)
-__all__.extend(pseudoRKQSStepper.__all__)
-__all__.extend(scaledStepper.__all__)
-__all__.extend(sequenceStepper.__all__)
+# Import all classes defined in modules into package namespace
+#
+# From https://julienharbulot.com/python-dynamical-import.html
+#
+# It's a more opaque way to say
+#
+#   from . import module
+#   from .module import *
+#   __all__.extend(module.__all__)
+#
+# OTOH, linters don't complain
+# and modules don't have to be listed manually
+
+# iterate through the modules in the current package
+package_dir = dirname(abspath(__file__))
+for (_, module_name, _) in iter_modules([package_dir]):
+
+    # import the module and iterate through its attributes
+    module = import_module("{}.{}".format(__name__, module_name))
+    for attribute_name in dir(module):
+        attribute = getattr(module, attribute_name)
+
+        if isclass(attribute):
+            # Add the class to this package's variables
+            globals()[attribute_name] = attribute
