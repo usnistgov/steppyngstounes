692961fb3238cb2bb759ff260f1d0d9ccee3181a
Jonathan Guyer <guyer@nist.gov>
2021-02-01 18:45:24 -0500

    Make imports from modules into package explicit
    [Explicit is better than implicit](python.org/dev/peps/pep-0020/)

--- steppyngstounes/__init__.py
+++ steppyngstounes/__init__.py
@@ -1,30 +1,6 @@
-from inspect import isclass
-from pkgutil import iter_modules
-from os.path import (abspath, dirname)
-from importlib import import_module
-
-# Import all classes defined in modules into package namespace
-#
-# From https://julienharbulot.com/python-dynamical-import.html
-#
-# It's a more opaque way to say
-#
-#   from . import module
-#   from .module import *
-#   __all__.extend(module.__all__)
-#
-# OTOH, linters don't complain
-# and modules don't have to be listed manually
-
-# iterate through the modules in the current package
-package_dir = dirname(abspath(__file__))
-for (_, module_name, _) in iter_modules([package_dir]):
-
-    # import the module and iterate through its attributes
-    module = import_module("{}.{}".format(__name__, module_name))
-    for attribute_name in dir(module):
-        attribute = getattr(module, attribute_name)
-
-        if isclass(attribute):
-            # Add the class to this package's variables
-            globals()[attribute_name] = attribute
+from .checkpointStepper import CheckpointStepper
+from .fixedStepper import FixedStepper
+from .pidStepper import PIDStepper
+from .pseudoRKQSStepper import PseudoRKQSStepper
+from .scaledStepper import ScaledStepper
+from .sequenceStepper import SequenceStepper
